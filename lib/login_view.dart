1119c9e8f9524dae64e1948d24a001b373ab5173
LiiNen <kjeonghoon065@gmail.com>
2022-03-31 22:35:44 +0900

    Add SplashView content

--- lib/login_view.dart
+++ lib/login_view.dart
@@ -2,8 +2,8 @@ import 'package:deleto/component/default_button.dart';
 import 'package:deleto/component/default_text_field.dart';
 import 'package:deleto/function.dart';
 import 'package:deleto/main_nav/main_nav_view.dart';
+import 'package:deleto/style.dart';
 import 'package:flutter/material.dart';
-import 'component/default_app_bar.dart';
 
 class LoginView extends StatefulWidget {
   @override
@@ -18,21 +18,29 @@ class _LoginView extends State<LoginView> {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: FocusManager.instance.primaryFocus?.unfocus,
-      child: Scaffold(
-        appBar: DefaultAppBar(title: '로그인'),
-        body: Container(
-          margin: EdgeInsets.symmetric(horizontal: 32),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              DefaultTextField(controller: idController, focusNode: idFocusNode, hint: 'ID', nextFocusNode: pwFocusNode,),
-              SizedBox(height: 24),
-              DefaultTextField(controller: pwController, focusNode: pwFocusNode, hint: 'PW', callback: loginAction),
-              SizedBox(height: 30),
-              DefaultButton(title: '로그인', callback: loginAction)
-            ]
+    return WillPopScope(
+      onWillPop: () => onWillPop(context),
+      child: GestureDetector(
+        onTap: FocusManager.instance.primaryFocus?.unfocus,
+        child: Scaffold(
+          body: Container(
+            margin: EdgeInsets.symmetric(horizontal: 32),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('DeliTo', style: textStyle(weight: 700, size: 24.0),),
+                SizedBox(height: 4),
+                Text('Delivery Together', style: textStyle(color: Color(0xffc9c9c9), weight: 500, size: 18.0)),
+                SizedBox(height: 12),
+                Text('배달팁에 대처하는 현명한 방법', style: textStyle(color: Color(0xffd5d5d5), weight: 400, size: 14.0)),
+                SizedBox(height: 24),
+                DefaultTextField(controller: idController, focusNode: idFocusNode, hint: 'ID', nextFocusNode: pwFocusNode,),
+                SizedBox(height: 24),
+                DefaultTextField(controller: pwController, focusNode: pwFocusNode, hint: 'PW', callback: loginAction),
+                SizedBox(height: 30),
+                DefaultButton(title: '로그인', callback: loginAction)
+              ]
+            )
           )
         )
       )
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:deleto/style.dart';
 import 'package:flutter/material.dart';
 import 'function.dart';
 import 'login_view.dart';
@@ -26,7 +27,7 @@ class _SplashView extends State<SplashView> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 1), pushLoginView);
+    Future.delayed(const Duration(seconds: 2), pushLoginView);
   }
 
   void pushLoginView() async {
@@ -35,8 +36,24 @@ class _SplashView extends State<SplashView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text('splash')
+    return WillPopScope(
+      onWillPop: () => onWillPop(context),
+      child: Scaffold(
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('DeliTo', style: textStyle(weight: 700, size: 24.0),),
+              SizedBox(height: 4),
+              Text('Delivery Together', style: textStyle(color: Color(0xffc9c9c9), weight: 500, size: 18.0)),
+              SizedBox(height: 12),
+              Text('배달팁에 대처하는 현명한 방법', style: textStyle(color: Color(0xffd5d5d5), weight: 400, size: 14.0)),
+              SizedBox(height: 30),
+            ]
+          )
+        )
+      )
     );
   }
 }
